eb583395810b873514760b7b552fd36f7b6df214
Nathan CHOPIN <nathan.chopin76@gmail.com>
2024-09-26 11:19:16 +0200

    dico complet

--- fn_TP2.py
+++ fn_TP2.py
@@ -4,7 +4,7 @@
 
 
 dictionnaire ={"le" : 0, "la" : 0, "chat" : 2, "souris" : 2, "martin" : 4,
-"mange" : 3, "la" : 0, "petite" : 1, "joli" : 1, "grosse" : 1,
+"mange" : 3, "petite" : 1, "joli" : 1, "grosse" : 1,
 "bleu" : 1, "verte" : 1, "dort" : 3,"julie" : 4, "jean" : 4, "." : 5}
 # article:0 , adjectif:1 , nom:2 , verbe:3 , nom propre:4 , ".":5
 
